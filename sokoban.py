65ffdf6cccdc196d9519260a3932236be4351df6
AliAzarafza <astinazarafza@gmail.com>
2023-12-16 15:44:56 -0500

    Implement breadthFirstSearch approach

--- sokoban.py
+++ sokoban.py
@@ -46,3 +46,115 @@ def TransferGameStateI(layout):
         if ColsNum < MaxColsNum:
             layout[IRow].extend([1 for _ in range(MaxColsNum - ColsNum)])
     return np.array(layout)
+
+def PosOfPlayer(GameState): # * Return the position of agent
+    return tuple(np.argwhere(GameState == 2)[0]) #! for example (2, 2)
+
+
+def PosOfBoxes(GameState): # * Return the positions of boxes
+    return tuple(tuple(x) for x in np.argwhere((GameState == 3) | (GameState == 5))) #! for example ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5))
+
+
+def PosOfWalls(GameState): # * Return the positions of walls
+    return tuple(tuple(x) for x in np.argwhere(GameState == 1)) #! like those above
+
+
+def PosOfGoals(GameState): # * Return the positions of goals
+    return tuple(tuple(x) for x in np.argwhere((GameState == 4) | (GameState == 5))) #! like those above
+
+
+def IsEndState(PosBox): # * Check if all boxes are on the goals (that is pass the game)
+    return sorted(PosBox) == sorted(PosGoals)
+
+def IsLeagalAction(action, PosPlayer, PosBox): # * Check if the given action is legal
+    xPlayer, yPlayer = PosPlayer
+    if action[-1].isupper(): #! the move was a push
+        x1 , y1 = xPlayer + 2 * action[0], yPlayer + 2 * action[1]
+    else:
+        x1, y1 = xPlayer + action[0], yPlayer + action[1]
+    return (x1,y1) not in PosBox + PosWalls
+
+def LeagalActions(PosPlayer, PosBox): # * Return all legal actions for the agent in the current game state
+    AllActions = [[-1,0,'u','U'],[1,0,'d','D'],[0,-1,'l','L'],[0,1,'r','R']]
+    xPlayer, yPlayer = PosPlayer
+    LeagalActions = []
+    for action in AllActions:
+        x1, y1 = xPlayer + action[0], yPlayer + action[1]
+        if (x1, y1) in PosBox: #! the move was a push
+            action.pop(2) #! drop the little letter
+        else:
+            action.pop(3) #! drop the upper letter
+        if IsLeagalAction(action, PosPlayer, PosBox):
+            LeagalActions.append(action)
+        else:
+            continue
+    return tuple(tuple(x) for x in LeagalActions) #! for example ((0, -1, 'l'), (0, 1, 'R'))
+
+
+def UpdateState(PosPlayer, PosBox, action): # * Return updated game state after an action is taken
+    xPlayer, yPlayer = PosPlayer #! the previous position of player
+    NewPosPlayer = [xPlayer + action[0], yPlayer + action[1]] #? the current position of player
+    PosBox = [list(x) for x in PosBox]
+    if action[-1].isupper(): #! if pushing, update the position of box
+        PosBox.remove(NewPosPlayer)
+        PosBox.append([xPlayer + 2 * action[0], yPlayer + 2 * action[1]])
+    PosBox = tuple(tuple(x) for x in PosBox)
+    NewPosPlayer = tuple(NewPosPlayer)
+    return NewPosPlayer, PosBox
+
+
+def IsFailed(PosBox): # * This function used to observe if the state is potentially failed, then prune the search
+    RotatePattern = [[0,1,2,3,4,5,6,7,8],
+                    [2,5,8,1,4,7,0,3,6],
+                    [0,1,2,3,4,5,6,7,8][::-1],
+                    [2,5,8,1,4,7,0,3,6][::-1]]
+    
+    FlipPattern = [[2,1,0,5,4,3,8,7,6],
+                    [0,3,6,1,4,7,2,5,8],
+                    [2,1,0,5,4,3,8,7,6][::-1],
+                    [0,3,6,1,4,7,2,5,8][::-1]]
+    
+    AllPatterns = RotatePattern + FlipPattern
+
+    for box in PosBox:
+        if box not in PosGoals:
+            board = [(box[0] - 1, box[1] - 1), (box[0] - 1, box[1]), (box[0] - 1, box[1] + 1), 
+                    (box[0], box[1] - 1), (box[0], box[1]), (box[0], box[1] + 1), 
+                    (box[0] + 1, box[1] - 1), (box[0] + 1, box[1]), (box[0] + 1, box[1] + 1)]
+            for pattern in AllPatterns:
+                NewBoard = [board[i] for i in pattern]
+                if NewBoard[1] in PosWalls and NewBoard[5] in PosWalls: return True
+                elif NewBoard[1] in PosBox and NewBoard[2] in PosWalls and NewBoard[5] in PosWalls: return True
+                elif NewBoard[1] in PosBox and NewBoard[2] in PosWalls and NewBoard[5] in PosBox: return True
+                elif NewBoard[1] in PosBox and NewBoard[2] in PosBox and NewBoard[5] in PosBox: return True
+                elif NewBoard[1] in PosBox and NewBoard[6] in PosBox and NewBoard[2] in PosWalls and NewBoard[3] in PosWalls and NewBoard[8] in PosWalls: return True
+        return False
+    
+    
+#* Implement all approcahes
+
+
+def BreadthFirstSearch(): #* Implement breadthFirstSearch approach
+    beginBox = PosOfBoxes(GameState)
+    beginPlayer = PosOfPlayer(GameState)
+
+    StartState = (beginPlayer, beginBox) #! for example ((2, 2), ((2, 3), (3, 4), (4, 4), (6, 1), (6, 4), (6, 5)))
+    frontier = collections.deque([[StartState]]) #! store states
+    actions = collections.deque([[0]]) #! store actions
+    ExplordSet = set()
+    while frontier:
+        node = frontier.popleft()
+        node_action = actions.popleft()
+        if IsEndState(node[-1][-1]):
+            print(','.join(node_action[1:]).replace(',',''))
+            break
+        if node[-1] not in ExplordSet:
+            ExplordSet.add(node[-1])
+            for action in LeagalActions(node[-1][0], node[-1][1]):
+                NewPosPlayer, NewPosBox = UpdateState(node[-1][0], node[-1][1], action)
+                if IsFailed(NewPosBox):
+                    continue
+                frontier.append(node + [(NewPosPlayer, NewPosBox)])
+                actions.append(node_action + [action[-1]])
+
+
